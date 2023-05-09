44420741038591bd1c84f6f961c3eb87ab20e3ad
Davis <taytaydavis88@yahoo.com>
2023-05-09 02:54:44 -0600

    First Commit, menue with 32 teams

---
+++ CustomProjections/ari.R
@@ -0,0 +1,9 @@
+ari_ui <- fluidPage(
+  # Add any UI elements here
+  h1("Welcome to the Arizona Cardinals page!")
+)
+
+# Define the server logic for the Arizona Cardinals tab
+ari_server <- function(input, output, session) {
+  # Add any server-side logic here
+}
---
+++ CustomProjections/server.R
@@ -0,0 +1,6 @@
+library(shiny)
+
+# Define the server logic
+server <- function(input, output) {
+  # No server-side logic needed for this example
+}
