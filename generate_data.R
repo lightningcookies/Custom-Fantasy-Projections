fcfbdc2e7e4bbc872949f5fa40447abffa59a0c9
Davis <taytaydavis88@yahoo.com>
2023-05-11 03:48:23 -0600

    Cleaned up data, added custom fantasy points for qb only. Added trgt_share stat. Changed file location for team stats.

--- generate_data.R
+++ generate_data.R
@@ -10,7 +10,13 @@ nfl_teams <- c("ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE",
                "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC",
                "LAC", "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG",
                "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS")
-
+# scoring parameters
+pr <- .5
+ptd <- 4
+pass_yd_pt <- .04
+int_point <- (-1)
+tp_conv <- 2
+fum_lost <- (-2)
 
 for(team in nfl_teams){
   QB_df <- df %>%
@@ -19,7 +25,8 @@ for(team in nfl_teams){
            season_type == "REG",
            recent_team == team)%>%
     group_by(player_name)%>%
-    summarise(attempts = sum(attempts),
+    summarise(position = unique(position),
+              attempts = sum(attempts),
               completions = sum(completions),
               comp_pct = completions/attempts,
               pass_yd = sum(passing_yards),
@@ -28,12 +35,18 @@ for(team in nfl_teams){
               rush_yd = sum(rushing_yards),
               rush_td = sum(rushing_tds),
               fumbles_lost = sum(rushing_fumbles_lost + sack_fumbles_lost),
+              two_point = sum(passing_2pt_conversions + rushing_2pt_conversions),
               fantasy_points = sum(fantasy_points),
               weeks_played = n())%>%
     mutate(comp_pct = percent(comp_pct, accuracy = 0.01)) %>%
     arrange(desc(fantasy_points))%>%
-    mutate(calc_fantasy_unfinished = (rush_td * 6)
-           + (pass_td * 6) + (.1 * rush_yd) + (.025 * pass_yd))
+    mutate(calc_fantasy_unfinished = (rush_td * 6) +
+             (.1 * rush_yd) + 
+             (pass_td * ptd) +
+             (pass_yd * pass_yd_pt) +
+             (int * int_point)+
+             (two_point * tp_conv)+
+             (fumbles_lost * fum_lost))
 
   RB_df <- df %>%
     filter(position == "RB",
@@ -41,16 +54,40 @@ for(team in nfl_teams){
            season_type == "REG",
            recent_team == team)%>%
     group_by(player_name)%>%
-    summarise(carries = sum(carries),
+    summarise(position = unique(position),
+              carries = sum(carries),
               rush_yd = sum(rushing_yards),
               rush_td = sum(rushing_tds),
+              trgt = sum(targets),
               receptions = sum(receptions),
               receiving_yards = sum(receiving_yards),
               fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
               fantasy_points = sum(fantasy_points),
               weeks_played = n())%>%
-    arrange(desc(fantasy_points))%>%
-    filter(carries > 5)
+    mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
+    #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
+    arrange(desc(fantasy_points))
+  
+  WR_df <- df %>%
+    filter(position == "WR",
+           season == 2022,
+           season_type == "REG",
+           recent_team == team)%>%
+    group_by(player_name)%>%
+    summarise(position = unique(position),
+              weeks_played = n(),
+              trgt = sum(targets),
+              receptions = sum(receptions),
+              rec_yards = sum(receiving_yards),
+              rec_td = sum(receiving_tds),
+              carries = sum(carries),
+              rush_yards = sum(rushing_yards),
+              rush_td = sum(rushing_tds),
+              fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
+              fantasy_ppr = sum(fantasy_points_ppr))%>%
+    mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
+    #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
+    arrange(desc(fantasy_ppr))
   
   TE_df <- df %>%
     filter(position == "TE",
@@ -58,7 +95,9 @@ for(team in nfl_teams){
            season_type == "REG",
            recent_team == team)%>%
     group_by(player_name)%>%
-    summarise(weeks_played = n(),
+    summarise(position = unique(position),
+              weeks_played = n(),
+              trgt = sum(targets),
               receptions = sum(receptions),
               rec_yds = sum(receiving_yards),
               rec_tds = sum(receiving_tds),
@@ -67,16 +106,25 @@ for(team in nfl_teams){
               rush_td = sum(rushing_tds),
               fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
               fantasy_ppr = sum(fantasy_points_ppr))%>%
+    mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
+    #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
     arrange(desc(fantasy_ppr))
 
   df_join1 <- full_join(QB_df,RB_df)
   df_join2 <- full_join(df_join1, TE_df)
-  write.csv(df_join2, paste0(team,".csv"))
-  print(df_join2)
+  df_join3 <- full_join(df_join2, WR_df)
+  filename <- paste0("team_data/", team, ".csv")
+  write.csv(df_join3, filename)
 }
 
+# By position ranks
+for(team in nfl_teams){
+  #QB
+  #RB
+  #WR
+}
 
-
+test_df <- read.csv("team_data/CIN.csv")
 
 # # OG ARIzona  only code
 # ARI_QB <- df %>%
@@ -110,11 +158,14 @@ for(team in nfl_teams){
 #   summarise(carries = sum(carries),
 #             rush_yd = sum(rushing_yards),
 #             rush_td = sum(rushing_tds),
+#             trgt = sum(targets),
 #             receptions = sum(receptions),
 #             receiving_yards = sum(receiving_yards),
 #             fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
 #             fantasy_points = sum(fantasy_points),
 #             weeks_played = n())%>%
+#   mutate(tgt_share = trgt/sum(ARI_QB$attempts))%>%
+#   mutate(tgt_share = percent(tgt_share, accuracy = 0.01))%>%
 #   arrange(desc(fantasy_points))%>%
 #   filter(carries > 5)%>%
 #   view()
@@ -126,6 +177,7 @@ for(team in nfl_teams){
 #          recent_team == "ARI")%>%
 #   group_by(player_name)%>%
 #   summarise(weeks_played = n(),
+#             trgt = sum(targets),
 #             receptions = sum(receptions),
 #             rec_yards = sum(receiving_yards),
 #             rec_td = sum(receiving_tds),
@@ -134,6 +186,8 @@ for(team in nfl_teams){
 #             rush_td = sum(rushing_tds),
 #             fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
 #             fantasy_ppr = sum(fantasy_points_ppr))%>%
+#   mutate(tgt_share = trgt/sum(ARI_QB$attempts))%>%
+#   mutate(tgt_share = percent(tgt_share, accuracy = 0.01))%>%
 #   arrange(desc(fantasy_ppr))%>%
 #   view()
 # 
