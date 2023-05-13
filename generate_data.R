0391ec9addeb9fbada6be45e252610414527097e
lightningcookies <82363426+lightningcookies@users.noreply.github.com>
2023-05-13 03:13:52 -0600

    Fixed LAR -> LA. Added combined csv. Fixed column name differences. finished off fantasy points?

--- generate_data.R
+++ generate_data.R
@@ -8,7 +8,7 @@ df <- read.csv("player_data.csv")
 
 nfl_teams <- c("ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE",
                "DAL", "DEN", "DET", "GB", "HOU", "IND", "JAX", "KC",
-               "LAC", "LAR", "LV", "MIA", "MIN", "NE", "NO", "NYG",
+               "LAC", "LA", "LV", "MIA", "MIN", "NE", "NO", "NYG",
                "NYJ", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS")
 # scoring parameters
 pr <- .5
@@ -27,13 +27,16 @@ df_filtered <- df %>%
          rushing_fumbles_lost, receiving_fumbles_lost,
          passing_yards, passing_tds, attempts, completions,
          interceptions, fantasy_points, fantasy_points_ppr, sack_fumbles_lost,
-         passing_2pt_conversions, rushing_2pt_conversions, carries)
+         passing_2pt_conversions, rushing_2pt_conversions,
+         receiving_2pt_conversions, carries)
 
 
 
 
 # main for loop
 for(team in nfl_teams){
+  
+  #QB
   QB_df <- df_filtered %>%
     filter(position == "QB",
            recent_team == team)%>%
@@ -49,10 +52,10 @@ for(team in nfl_teams){
               rush_td = sum(rushing_tds),
               fumbles_lost = sum(rushing_fumbles_lost + sack_fumbles_lost),
               two_point = sum(passing_2pt_conversions + rushing_2pt_conversions),
-              fantasy_points = sum(fantasy_points),
+              fantasy_ppr = sum(fantasy_points_ppr),
               weeks_played = n())%>%
     mutate(comp_pct = percent(comp_pct, accuracy = 0.01)) %>%
-    arrange(desc(fantasy_points))%>%
+    arrange(desc(fantasy_ppr))%>%
     mutate(calc_fantasy_unfinished = (rush_td * 6) +
              (.1 * rush_yd) + 
              (pass_td * ptd) +
@@ -60,7 +63,8 @@ for(team in nfl_teams){
              (int * int_point)+
              (two_point * tp_conv)+
              (fumbles_lost * fum_lost))
-
+  
+# RB
   RB_df <- df_filtered %>%
     filter(position == "RB",
            recent_team == team)%>%
@@ -71,14 +75,24 @@ for(team in nfl_teams){
               rush_td = sum(rushing_tds),
               trgt = sum(targets),
               receptions = sum(receptions),
-              receiving_yards = sum(receiving_yards),
+              rec_yd = sum(receiving_yards),
+              rec_td = sum(receiving_tds),
+              two_point = sum(receiving_2pt_conversions + rushing_2pt_conversions),
               fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
-              fantasy_points = sum(fantasy_points),
+              fantasy_ppr = sum(fantasy_points_ppr),
               weeks_played = n())%>%
     mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
     #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
-    arrange(desc(fantasy_points))
-  
+    arrange(desc(fantasy_ppr))%>%
+    mutate(calc_fantasy_unfinished = (rush_td * 6) +
+             (.1 * rush_yd) + 
+             (pr * receptions) +
+             (.1 * rec_yd) +
+             (6 * rec_td) +
+             (tp_conv * two_point) +
+             (fum_lost * fumbles_lost))
+             
+  # WR
   WR_df <- df_filtered %>%
     filter(position == "WR",
            recent_team == team)%>%
@@ -87,17 +101,26 @@ for(team in nfl_teams){
               weeks_played = n(),
               trgt = sum(targets),
               receptions = sum(receptions),
-              rec_yards = sum(receiving_yards),
+              rec_yd = sum(receiving_yards),
               rec_td = sum(receiving_tds),
               carries = sum(carries),
-              rush_yards = sum(rushing_yards),
+              rush_yd = sum(rushing_yards),
               rush_td = sum(rushing_tds),
+              two_point = sum(receiving_2pt_conversions + rushing_2pt_conversions),
               fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
               fantasy_ppr = sum(fantasy_points_ppr))%>%
     mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
     #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
-    arrange(desc(fantasy_ppr))
-  
+    arrange(desc(fantasy_ppr))%>%
+    mutate(calc_fantasy_unfinished = (rush_td * 6) +
+             (.1 * rush_yd) + 
+             (pr * receptions) +
+             (.1 * rec_yd) +
+             (6 * rec_td) +
+             (tp_conv * two_point) +
+             (fum_lost * fumbles_lost))
+    
+  #TE
   TE_df <- df_filtered %>%
     filter(position == "TE",
            recent_team == team)%>%
@@ -106,16 +129,25 @@ for(team in nfl_teams){
               weeks_played = n(),
               trgt = sum(targets),
               receptions = sum(receptions),
-              rec_yds = sum(receiving_yards),
-              rec_tds = sum(receiving_tds),
+              rec_yd = sum(receiving_yards),
+              rec_td = sum(receiving_tds),
               carries = sum(carries),
-              rush_yards = sum(rushing_yards),
+              rush_yd = sum(rushing_yards),
               rush_td = sum(rushing_tds),
+              two_point = sum(receiving_2pt_conversions + rushing_2pt_conversions),
               fumbles_lost = sum(rushing_fumbles_lost + receiving_fumbles_lost),
               fantasy_ppr = sum(fantasy_points_ppr))%>%
     mutate(trgt_share = trgt/sum(QB_df$attempts))%>%
     #mutate(tgt_share = percent(as.numeric(tgt_share), accuracy = 0.01))%>%
-    arrange(desc(fantasy_ppr))
+    arrange(desc(fantasy_ppr))%>%
+    mutate(calc_fantasy_unfinished = (rush_td * 6) +
+             (.1 * rush_yd) + 
+             (pr * receptions) +
+             (.1 * rec_yd) +
+             (6 * rec_td) +
+             (tp_conv * two_point) +
+             (fum_lost * fumbles_lost))
+    
 
   df_join1 <- full_join(QB_df,RB_df)
   df_join2 <- full_join(df_join1, TE_df)
@@ -131,7 +163,48 @@ for(team in nfl_teams){
   #WR
 }
 
-test_df <- read.csv("team_data/CIN.csv")
+test_df <- read.csv("team_data/LA.csv")
+
+
+
+# Get a list of all the CSV files in the folder
+csv_files <- list.files("./team_data", pattern = "*.csv")
+
+setwd("./team_data")
+
+df_list <- lapply(csv_files, read_csv)
+
+df <- bind_rows(df_list)
+setwd("..")
+write_csv(df, "combined_data.csv")
+
+
+
+
+# Combined Df
+df_c <- read.csv("combined_data.csv")
+
+# By position finish rankings
+qb_c <- df_c %>%
+  filter(position == "QB")%>%
+  group_by(player_name)%>%
+  arrange(desc(fantasy_ppr))%>%
+  view()
+
+rb_c <- df_c %>%
+  filter(position == "RB")%>%
+  group_by(player_name)%>%
+  arrange(desc(fantasy_ppr))%>%
+  view()
+
+wr_c <- df_c %>%
+  filter(position == "WR")%>%
+  group_by(player_name)%>%
+  arrange(desc(fantasy_ppr))%>%
+  view()
+
+
+
 
 # # OG ARIzona  only code
 # ARI_QB <- df %>%
